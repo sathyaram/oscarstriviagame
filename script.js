a507c8cf789f006c47db0fa6a152253ab5ce956f
Sathya Ram <sathyatheram@gmail.com>
2018-09-11 15:32:25 -0400

    created index.html, script.js, and styles.css - threw html5 boilerplate, and added an array with some questionanswer items

---
+++ script.js
@@ -0,0 +1,33 @@
+// Questions, Nominees and Answers
+let quiz2018 = [
+    // Best Picture
+    { 
+    question: 'Which movie won Best Picture?', 
+    nominees: { one: "Call Me By Your Name", two: "Darkest Hour", three: "Dunkirk", four: "Get Out", five: "Lady Bird", six: "Phantom Thread", seven: "The Post", eight: "The Shape of Water", nine: "Three Billboards Outside Ebbing, Missouri"},
+    answer: "The Shape of Water"
+    },
+    // Best Actor
+    {
+    question: 'Which actor won Best Actor?',
+    nominees: { one: "Timothee Chalamet", two: "Daniel Day-Lewis", three: "Daniel Kaluuya", four: "Gary Oldman", five: "Denzel Washington"},
+    answer: "Gary Oldman"
+    },
+    // Best Actress
+    {
+    question: 'Which actress won Best Actress?',
+    nominees: { one: "Sally Hawkins", two: "Frances McDormand", three: "Margot Robbie", four: "Saoirse Ronan", five: "Meryl Steep"},
+    answer: "Frances McDormand"
+    },
+    // Best Director
+    {
+    question: 'Which director won Best Director?'
+    },
+    // Best Supporting Actor
+    {
+
+    },
+    // Best Supporting Actress
+    {
+
+    }
+];
